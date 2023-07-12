d051a5af6d121142c6ce83dc47c2db7c91122517
Alice Hepburn <aliceokhepburn@gmail.com>
2023-07-12 14:17:03 -0400

    DELIVERABLE THREE passes. Search through stores database.

--- src/App.js
+++ src/App.js
@@ -1,61 +1,89 @@
-import './App.css';
-import Search from './components/Search'
-import NewStoreForm from './components/NewStoreForm';
-import StoreList from './components/StoreList';
-import {useState, useEffect} from 'react'
+import "./App.css";
+import Search from "./components/Search";
+import NewStoreForm from "./components/NewStoreForm";
+import StoreList from "./components/StoreList";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [stores, setStores] = useState([])
+  const [stores, setStores] = useState([]);
 
-  const [name, setName] = useState('')
-    const [image, setImage] = useState('')
-    const [season, setSeason] = useState(0)
-    const [episode, setEpisode] = useState (0)
+  //▼▼▼▼▼ allows text you're entering to be tracked
+  const [searchText, setSearchText] = useState("");
 
-  useEffect(()=>{
-    fetch('http://localhost:8085/stores')
-    .then(res=>res.json())
-    .then(stores=>setStores(stores))
-  }, [])
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [season, setSeason] = useState(0);
+  const [episode, setEpisode] = useState(0);
 
-  function post(e){
-    e.preventDefault()
-    fetch('http://localhost:8085/stores', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          name: name,
-          image: image,
-          season: season,
-          episode: episode,
-        }
-        )
+  //GET request for basic access to JSON data
+  useEffect(() => {
+    fetch("http://localhost:8085/stores")
+      .then((res) => res.json())
+      .then((stores) => setStores(stores));
+  }, []);
+
+  //POST fetch for adding a new store from the form
+  function post(e) {
+    e.preventDefault();
+    fetch("http://localhost:8085/stores", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        image: image,
+        season: season,
+        episode: episode,
+      }),
     })
-    .then(res=>res.json())
-    //.then(data=>console.log(data))
-    .then(newStore =>{setStores([...stores, newStore])})
-    
+      .then((res) => res.json())
+      //.then(data=>console.log(data))
+      .then((newStore) => {
+        setStores([...stores, newStore]);
+      });
   }
+
+  //▼▼▼▼▼ updates the searchText state with the input from Search.js and the search-content div
+  //must be passed as a prop to the Search component to retrieve the input value
+  function updateSearchText(event) {
+    setSearchText(event.target.value);
+  }
+
+  //▼▼▼▼▼ is the array that returns searched words
+  //this has to be passed to StoreList instead of just Stores so the table changes
+  const filteredStores = stores.filter((store) =>
+    //has to be an if else statement of what matches the searchText
+    //why do I need a if else
+    //stores.filter((store)=>{return store.name.toLowerCase().includes(searchText.toLowerCase())
+    {if(searchText === ''){
+      return true
+      //▲▲▲▲ why do we set it as true? how does that keep it at full stores. why not return 'stores'?
+    }else{
+      return store.name.toLowerCase().includes(searchText.toLowerCase())
+      //▲▲▲▲Have to use .toLowerCase, human error is real, I was ready to spiral over why the search results were not accurate
+    }
+  }
+  );
 
   return (
     <div className="main-container">
       <img src="/images/bobsburgers.png" />
       <h1>Neighbor Stores</h1>
-      <Search />
+      <Search updateSearchText={updateSearchText} />
+      {/* ▼▼▼▼ seriously can I not write my states in NewStoreForm and NOT pass 9 props*/}
       <NewStoreForm
-      name={name}
-      image={image}
-      season={season}
-      episode={episode}
-      setName={setName}
-      setImage={setImage}
-      setSeason={setSeason}
-      setEpisode={setEpisode}
-      post = {post}
+        name={name}
+        image={image}
+        season={season}
+        episode={episode}
+        setName={setName}
+        setImage={setImage}
+        setSeason={setSeason}
+        setEpisode={setEpisode}
+        post={post}
       />
-      <StoreList stores={stores}/>
+      <StoreList stores={filteredStores} />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,10 +4,11 @@ import React from "react"
 Filter stores by typing into the search bar. Only stores with a name matching the search term should be store in the table.
  */
 
-function Search() {
+function Search({updateSearchText}) {
     return(
         <div className="search-container">
-            <input type="text" placeholder="Search names..." onChange={() => console.log("Searching...")} />
+            <input type="text" placeholder="Search names..."
+            onChange={(event) => updateSearchText(event)} />
         </div>
     );
 }
